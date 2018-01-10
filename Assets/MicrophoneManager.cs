cc40290e3566bcb7b5423d84956a89539ae6c872
Nick Young <nyou045@UoA.auckland.ac.nz>
2018-01-10 18:19:01 +1300

    remove watson sdk, refactor to use UnityWebRequest

--- Assets/MicrophoneManager.cs
+++ Assets/MicrophoneManager.cs
@@ -1,10 +1,8 @@
-﻿using IBM.Watson.DeveloperCloud.Connection;
-using IBM.Watson.DeveloperCloud.Services.ToneAnalyzer.v3;
-using IBM.Watson.DeveloperCloud.Utilities;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.Networking;
 using UnityEngine.UI;
 using UnityEngine.Windows.Speech;
 
@@ -13,7 +11,6 @@ public class MicrophoneManager : MonoBehaviour
 
 	public Text debug;
 	private DictationRecognizer dictationRecognizer;
-	private ToneAnalyzer toneAnalyzer;
 	private Dictionary<string, Color> emotionColors;
 	private Material mat;
 	private List<Color> currentColors;
@@ -23,11 +20,6 @@ public class MicrophoneManager : MonoBehaviour
 	// Use this for initialization
 	void Start()
 	{
-		var credentials = new Credentials("f0fa6e61-82e7-4bf6-adeb-7392e0253c8b", "T8lCuYlwEeVI", "https://gateway.watsonplatform.net/tone-analyzer/api");
-
-		//{ "document_tone":{ "tone_categories":[{"tones":[{"score":0.015361,"tone_id":"anger","tone_name":"Anger"},{"score":0.007565,"tone_id":"disgust","tone_name":"Disgust"},{"score":0.015474,"tone_id":"fear","tone_name":"Fear"},{"score":0.898416,"tone_id":"joy","tone_name":"Joy"},{"score":0.023349,"tone_id":"sadness","tone_name":"Sadness"}],"category_id":"emotion_tone","category_name":"Emotion Tone"},
-		//{ "tones":[{"score":0.0,"tone_id":"analytical","tone_name":"Analytical"},{"score":0.0,"tone_id":"confident","tone_name":"Confident"},{"score":0.0,"tone_id":"tentative","tone_name":"Tentative"}],"category_id":"language_tone","category_name":"Language Tone"},
-		//{ "tones":[{"score":0.064835,"tone_id":"openness_big5","tone_name":"Openness"},{"score":0.276417,"tone_id":"conscientiousness_big5","tone_name":"Conscientiousness"},{"score":0.613644,"tone_id":"extraversion_big5","tone_name":"Extraversion"},{"score":0.363286,"tone_id":"agreeableness_big5","tone_name":"Agreeableness"},{"score":0.785214,"tone_id":"emotional_range_big5","tone_name":"Emotional Range"}],"category_id":"social_tone","category_name":"Social Tone"}]}}
 		emotionColors = new Dictionary<string, Color>()
 		{
 			{"anger", Color.red},
@@ -45,9 +37,7 @@ public class MicrophoneManager : MonoBehaviour
 
 		mat = gameObject.GetComponentInChildren<Renderer>().material;
 
-		toneAnalyzer = new ToneAnalyzer(credentials);
-		toneAnalyzer.VersionDate = "2017-05-26";
-		//toneAnalyzer.GetToneAnalyze(OnGetToneAnalyze, OnFail, "I hate these new features On #ThisPhone after the update.");
+		//StartCoroutine(GetToneAnalysis("I'm happy"));
 
 		dictationRecognizer = new DictationRecognizer(ConfidenceLevel.Rejected);
 		dictationRecognizer.AutoSilenceTimeoutSeconds = 5;
@@ -78,12 +68,7 @@ public class MicrophoneManager : MonoBehaviour
 			}
 			else
 			{
-				if (!toneAnalyzer.GetToneAnalyze(OnGetToneAnalyze, OnFail, text))
-				{
-					outText = "failed to analyze tone";
-					Debug.Log(outText);
-					debug.text = outText;
-				}
+				StartCoroutine(GetToneAnalysis(text));
 			}
 		};
 
@@ -119,32 +104,40 @@ public class MicrophoneManager : MonoBehaviour
 		mat.color = Color.Lerp(currentColors[a], currentColors[b], t);
 	}
 
-	private void OnGetToneAnalyze(ToneAnalyzerResponse resp, Dictionary<string, object> customData)
+	IEnumerator GetToneAnalysis(string text)
 	{
-		var outText = "tone analyzer response: \n";
-		var toneCats = resp.document_tone.tone_categories.ToDictionary(x => x.category_id, x => x.tones);
-		var significantEmotions = toneCats["emotion_tone"].Where(x => x.score > .5f);
-		currentColors = significantEmotions.Select(x => emotionColors[x.tone_id]).ToList();
-		if (significantEmotions.Count() == 0)
-		{
-			currentColors = defaultColors;
-		}
-		else if (significantEmotions.Count() == 1)
-		{
-			currentColors.Add(Color.white);
-		}
-		foreach (var emotion in significantEmotions)
-		{
-			outText += emotion.tone_id + ": " + emotion.score + "=" + emotionColors[emotion.tone_id] + "\n";
-		}
-		Debug.Log(outText);
-		debug.text = outText;
-	}
+		var www = UnityWebRequest.Post("https://gateway.watsonplatform.net/tone-analyzer/api/v3/tone?version=2018-01-01&sentences=false", text);
+		www.SetRequestHeader("Authorization", "Basic ZjBmYTZlNjEtODJlNy00YmY2LWFkZWItNzM5MmUwMjUzYzhiOlQ4bEN1WWx3RWVWSQ==");
+		www.SetRequestHeader("Content-Type", "text/plain");
+		yield return www.SendWebRequest();
 
-	private void OnFail(RESTConnector.Error error, Dictionary<string, object> customData)
-	{
-		var outText = "tone analyzer error: " + error.ToString();
-		Debug.Log(outText);
-		debug.text = outText;
+		if (www.isNetworkError || www.isHttpError)
+		{
+			var outText = www.error;
+			debug.text = outText;
+			Debug.LogError(outText);
+		}
+		else
+		{
+			string responseString = www.downloadHandler.text;
+			var json = new JSONObject(responseString);
+			var tones = json["document_tone"]["tones"].list;
+			var outText = "tone analyzer response: \n";
+			currentColors = tones.Select(x => emotionColors[x["tone_id"].str]).ToList();
+			if (tones.Count() == 0)
+			{
+				currentColors = defaultColors;
+			}
+			else if (tones.Count() == 1)
+			{
+				currentColors.Add(Color.white);
+			}
+			foreach (var emotion in tones)
+			{
+				outText += emotion["tone_id"].str + ": " + emotion["score"].f + "=" + emotionColors[emotion["tone_id"].str] + "\n";
+			}
+			Debug.Log(outText);
+			debug.text = outText;
+		}
 	}
 }
